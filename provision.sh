0e5d39782ad33ef6a01adb6a497f68321adffc2a
curious725 <curious725@gmail.com>
2017-06-28 11:14:27 +0300

    install node in provision

--- provision.sh
+++ provision.sh
@@ -12,3 +12,13 @@ export LC_ALL=en_US.UTF-8">>~/.bashrc
 locale-gen en_US.UTF-8
 sudo dpkg-reconfigure locales
 source ~/.bashrc
+
+# install node
+dpkg -s nodejs &>/dev/null || {
+	curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+	apt-get install -y nodejs
+	apt-get install -y build-essential
+
+	# ln -s /usr/bin/nodejs /usr/bin/node
+}
+
